fc1d23b8f24deb645439009d3bb8ff912c32e177
Mathew Lee <mwlee9@gmail.com>
2018-01-28 12:30:15 -0600

    Added stylesheets and static file serving

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 	models.CreateTable()
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	// Handlers
 	router := httprouter.New()
@@ -24,6 +24,8 @@ func main() {
 	router.GET("/item/:id/", handlers.GetOneTask) //REMEMBER: a req without an ID still routes to this URL, passing in "favicon.ico" as the param. can't be /:id, must be diff branch url/item/:id
 	router.DELETE("/item/:id", handlers.DeleteOneTask)
 	router.POST("/", handlers.NewTask)
+	router.ServeFiles("/static/*filepath", http.Dir("static"))
+	// REMEMBER: Chrome caches the stylesheet when it can't be found...use firefox instead, or delete history in chrome.
 
 	http.ListenAndServe(":8000", router)
 }
